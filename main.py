cacc38b7ff904cdd815c97830875a9fab9dc75ed
kolesov-alexandr <aleks.kolesov51@gmail.com>
2022-03-31 15:27:47 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Тетрис")
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+
+
+if __name__ == '__main__':
+    main()
